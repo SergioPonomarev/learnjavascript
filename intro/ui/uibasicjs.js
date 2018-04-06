907122c559c2c5811da6cb6f9cca76d413ac17db
Tahmasib <sergiopono@gmail.com>
2018-04-06 03:33:11 +0300

    commenting first prompt, alert and confirm. Adding prompt and alert with name

--- intro/ui/uibasicjs.js
+++ intro/ui/uibasicjs.js
@@ -1,4 +1,8 @@
+/*
 let years = prompt('how old are you?', '100');
 alert('you are ' + years + ' years old');
 
-confirm('Are you admin?');
+confirm('Are you admin?');*/
+
+let name = prompt('what is your name?', '');
+alert('Hello ' + name + '!');
