7302dbd36daecb64530343b4cb8d828ae0e14bb8
Tahmasib <sergiopono@gmail.com>
2018-04-11 01:48:00 +0300

    check out of range

--- intro/4logical-ops/logical-ops-js.js
+++ intro/4logical-ops/logical-ops-js.js
@@ -1,6 +1,13 @@
 let age = +prompt('pick a number', '');
 
+/*
 if (age >= 14 && age <= 90) {
+    alert('correct');
+} else {
+    alert('not good');
+}*/
+
+if (!age >= 14 && !age <= 90) {
     alert('correct');
 } else {
     alert('not good');
