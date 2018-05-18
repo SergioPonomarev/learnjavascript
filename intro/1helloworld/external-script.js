f478f3362c40761cb9329a02091847b3faf23e67
Tahmasib <sergiopono@gmail.com>
2018-05-18 15:21:41 +0400

    add new chapter Function basics

--- intro/1helloworld/external-script.js
+++ intro/1helloworld/external-script.js
@@ -1,1 +1,18 @@
 console.log('external I\'am JavaScript');
+
+function foo(lines) {
+    var star = '*';
+    var t = 0;
+    while(t < lines) {
+        var space = ' ';
+        for (var i = 0; i < lines - t; i++) {
+            space += ' ';
+        }
+        console.log(space + star);
+        star += '**';
+        t++;
+    }
+}
+ 
+
+foo(5);
